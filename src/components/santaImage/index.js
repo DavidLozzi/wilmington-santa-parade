f212cef5b444bb9e8bc4ae61849a41fe7eedaf93
David Lozzi <david@lozzi.net>
2023-12-01 15:52:43 -0500

    updates for 2024

--- src/components/santaImage/index.js
+++ src/components/santaImage/index.js
@@ -3,10 +3,10 @@ import './index.css';
 
 const SantaImage = () => {
 
-    return <div id="hero">
-        <img src={SantaPng} id="logo" alt="Wilmington Santa Parade Logo" />
-        <h2>December 4th starting at 9am</h2>
-        </div>
+  return <div id="hero">
+    <img src={SantaPng} id="logo" alt="Wilmington Santa Parade Logo" />
+    <h2>December 3rd starting at 8:30am</h2>
+  </div>
 }
 
 export default SantaImage;
